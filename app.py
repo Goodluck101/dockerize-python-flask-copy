a5317ce67d0599cf1c60d9b0404f21c9cd9dd086
Goodluck <ekeomaadiole@gmail.com>
2024-07-17 22:45:40 +0100

    committing dockerized python app

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, abort
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    html = f"<h1>Hello World!</h1>"
+    html = f"<h1>Hello World!</h1> This is Version: 1.0.1"
     return html
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=3000)
+	app.run(host="0.0.0.0", port=5000)
